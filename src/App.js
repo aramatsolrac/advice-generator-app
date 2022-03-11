d4b231a95392d5f84d1e4e2bb8493bc3bf4b530a
Ramgarhia007 <jaggaramgarhia007@gmail.com>
2022-03-11 16:12:50 -0500

    styling sheet

--- src/App.js
+++ src/App.js
@@ -1,49 +1,48 @@
-import { Component } from 'react';
-import axios from 'axios'
-import './App.css';
+import { Component } from "react";
+import axios from "axios";
+import "./app.scss";
+import dice from './images/dive.svg';
 
-class App extends Component { 
+class App extends Component {
   state = {
     quotes: [],
   };
 
-  componentDidMount(){
+  componentDidMount() {
     this.getQuotesData();
   }
 
   getQuotesData = () => {
     axios
-    .get(`${process.env.REACT_APP_API_URL}/quotes`)
-    .then((response) => {
+      .get(`${process.env.REACT_APP_API_URL}/quotes`)
+      .then((response) => {
         this.setState({
           quotes: response.data.advice,
-        })
-
-  })
-    .catch((err) =>{
-      console.log('getting error')
-    });
+        });
+      })
+      .catch((err) => {
+        console.log("getting error");
+      });
   };
 
   render() {
-    return(
-      <div>
-
-      <div>
-      <p> {this.state.quotes}</p>
-      </div>
-
-      <div>
-        <button type='button' onClick={this.getQuotesData}>
-          Another
+    return (
+      <div className="box">
+        <div className="box__quote">
+          <div className="box__quote-text">
+            <p> {this.state.quotes}</p>
+          </div>
+        </div>
+        <button
+              className="box__button"
+              type="button"
+              onClick={this.getQuotesData}
+            >
+             <img className="box__button-img" src={dice} alt="dice img"></img> 
         </button>
       </div>
-    </div>   
-      
     );
-  };
-  
-};
-
+  }
+}
 
 export default App;
